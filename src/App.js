b8555936f2aa4a01bedb53268fb524ce7b28d7f7
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-03-04 10:14:21 +0100

    I test the Drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "./components/header";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
+
+    /* <Router>
+        <Route path="/drawer" component={TemporaryDrawer} />
+      </Router> */
   );
 }
 
