5e7b8927106289f1176b4b9a06d380bafec7545f
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-03-26 06:57:42 -0400

    I am implementing Netlify form handling

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,6 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./form.styles.scss";
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 class ContactForm extends Component {
   constructor(props) {
     super(props);
@@ -36,6 +41,8 @@ class ContactForm extends Component {
     });
   };
 
+  // handleChange = e => this.setState({ [e.target.fullName]: e.target.value });
+
   resetForm = () => {
     this.setState({
       fullName: "",
@@ -46,61 +53,96 @@ class ContactForm extends Component {
   };
 
   handleSubmit = e => {
-    e.preventDefault();
-    const templateId = "contact_form";
-    this.sendFeedback(templateId, {
-      subject: this.state.subject,
-      message: this.state.message,
-      fullName: this.state.fullName,
-      reply_to: this.state.email
-    });
-    this.resetForm();
-  };
-
-  sendFeedback(templateId, variables) {
-    window.emailjs
-      .send("gmail", templateId, variables)
-      .then(() => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "message", ...this.state })
+    })
+      .then(() =>
         swal({
           title: "Thank you!",
           text: "Message sent successfully,\n We'll get back to you shortly.",
           icon: "success"
-        });
-      })
-      .catch(() => {
+        })
+      )
+      .catch(error =>
         swal({
-          title: "Oooooops!",
-          text: "Something went wrong, please try again.",
-          icon: "warning",
-          dangerMode: true
-        });
-      });
-  }
+          title: "Ooooops!",
+          text: error,
+          icon: "danger"
+        })
+      );
+
+    e.preventDefault();
+  };
+
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   const templateId = "contact_form";
+  //   this.sendFeedback(templateId, {
+  //     subject: this.state.subject,
+  //     message: this.state.message,
+  //     fullName: this.state.fullName,
+  //     reply_to: this.state.email
+  //   });
+  //   this.resetForm();
+  // };
+
+  // sendFeedback(templateId, variables) {
+  //   window.emailjs
+  //     .send("gmail", templateId, variables)
+  //     .then(() => {
+  //       swal({
+  //         title: "Thank you!",
+  //         text: "Message sent successfully,\n We'll get back to you shortly.",
+  //         icon: "success"
+  //       });
+  //     })
+  //     .catch(() => {
+  //       swal({
+  //         title: "Oooooops!",
+  //         text: "Something went wrong, please try again.",
+  //         icon: "warning",
+  //         dangerMode: true
+  //       });
+  //     });
+  // }
 
   render() {
+    const { fullName, email, subject, message } = this.state;
     return (
-      <form className="contact-form">
+      <form
+        className="contact-form"
+        method="POST"
+        onSubmit={this.handleSubmit}
+        data-netlify="true"
+        name="message"
+      >
+        {/* <input type="hidden" name="form-name" value="message" /> */}
         <input
           type="text"
           placeholder="Full Name"
+          name="fullName"
           className="form-input"
-          value={this.state.fullName}
+          value={fullName}
           onChange={this.updateName}
           required
         />
         <input
           type="email"
           placeholder="Email Address"
+          name="email"
           className="form-input"
-          value={this.state.email}
+          value={email}
           onChange={this.updateEmail}
           required
         />
         <input
           type="text"
           placeholder="Subject"
+          name="subject"
           className="form-input"
-          value={this.state.subject}
+          value={subject}
           onChange={this.updateSubject}
           required
         />
@@ -108,15 +150,12 @@ class ContactForm extends Component {
           cols="5"
           rows="5"
           placeholder="Your Message"
-          value={this.state.message}
+          name="message"
+          value={message}
           onChange={this.updateMessage}
           required
         ></textarea>
-        <button
-          type="submit"
-          onClick={e => this.handleSubmit(e)}
-          className="btn-contact"
-        >
+        <button type="submit" className="btn-contact">
           Send Message &nbsp;
           <FontAwesomeIcon icon={faArrowRight} />
         </button>
