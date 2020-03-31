216c94789cbcf580fb46330b491fbbb121990ef1
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-03-31 13:38:54 +0100

    I create an array of testimonial data

---
+++ src/components/carousel/carouselData.js
@@ -0,0 +1,47 @@
+const testimonialMessages = [
+  {
+    id: 1,
+    imgUrl: `https://res.cloudinary.com/john-pels/image/upload/v1566911465/devlogo2.png`,
+    testitfierName: "Akintunde Sultan",
+    testifierMessage: `John is a very fast leaner, he has achieved in few months what others would in 2 years.His consistency is amazing and how we was able to keep growing his skill while still being a part of a startup’s core team is wonderful. He’s great with leading and awesome with collaborating.`
+  },
+  {
+    id: 2,
+    imgUrl: `https://res.cloudinary.com/john-pels/image/upload/v1585119611/Chris.jpg`,
+    testitfierName: "Chris Blakely",
+    testifierMessage: `John is an incredibly talented frontend developer who can turn designs into pixel- perfect code.He is able understand complex problems quickly and write well designed code to solve these problems.John is a natural leader who can communicate well with his teammates and clients.`
+  },
+  {
+    id: 3,
+    imgUrl: `https://res.cloudinary.com/john-pels/image/upload/v1584945486/darling.jpg`,
+    testitfierName: "Oyekanmi Tofunmi",
+    testifierMessage: `Believe me! Mr John is that excellent software developer you have been looking to get on your team but it only takes you hiring him  for that project/ job for you to know how blessed you are for such a rare gift as him.`
+  },
+  {
+    id: 4,
+    imgUrl: `https://res.cloudinary.com/john-pels/image/upload/v1584949054/sadiq.jpg`,
+    testitfierName: "Adeyemi Sadiq",
+    testifierMessage: `I’ve known and mentored John for the past five years. I’ve always
+            been impressed with his ability to handle projects while at the
+            university. He is always motivated to handle software tasks and his
+            expertise in how he handles the colossal task and breaks it down to
+            chunks.John would be an excellent addition to your team. Please feel
+            free to contact me by Email
+            <strong>
+              (sadiq.adeyemi@gplustechsolutions.com) or (+2347062605399)
+            </strong>`
+  },
+  {
+    id: 5,
+    imgUrl: `https://res.cloudinary.com/john-pels/image/upload/v1585049073/Chidi.jpg`,
+    testitfierName: "Chidi Okoye",
+    testifierMessage: ` John's ability to come up with simple, yet efficient solutions to
+            complex problems is definitely something I admire so much about him.
+            His great dedication to learning and self-improvement is also very
+            commendable. He is a really good team player, and has huge impact on
+            both team efficiency and morale. Definitely a pleasure to work with
+            him.`
+  }
+];
+
+export default testimonialMessages;
