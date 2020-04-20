59f15c850a596edcbb47f70e81d5812ed14a0bf7
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-20 13:45:12 +0100

    I remove the inline-style

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,18 +1,11 @@
 import React, { useContext } from "react";
 import Context from "../Context";
 
-const Styles = {
-  fontSize: ".2em",
-  color: "#090833",
-};
-
 const Content = () => {
   const { state } = useContext(Context);
 
   return (
-    <span styles={Styles} className="mr-2">{`${
-      state.isDark ? "Light" : "Dark"
-    } mode `}</span>
+    <span className="mr-2">{`${state.isDark ? "Light" : "Dark"} mode `}</span>
   );
 };
 
