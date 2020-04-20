e3af87532f7315857aa528210e4637775569ecf4
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-19 20:07:28 -0400

    I refactor the structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyles";
 import Homepage from "./pages/homepage";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Dark, Light } from "./components/Themes";
+import Context from "./components/Context";
+import reducer from "./components/Reducer";
 
-function App() {
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    isDark: false,
+  });
   return (
     <Router>
-      <div className="App">
-        <Homepage />
-      </div>
+      <Context.Provider value={{ state, dispatch }}>
+        <ThemeProvider theme={state.isDark ? Dark : Light}>
+          <>
+            <div className="App">
+              <GlobalStyles />
+              <Homepage />
+            </div>
+          </>
+        </ThemeProvider>
+      </Context.Provider>
     </Router>
   );
 }
-
-export default App;
